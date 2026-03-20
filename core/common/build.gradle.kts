4226d5ea2921ef145fe8a1a5a076eeb0a65698b2
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-03-20 22:56:40 +0100

    Merge and cleanup version catalogs (#3103)
    (cherry picked from commit e930f38608c691b01c5f7e20c05b8a46566ab169)

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -28,24 +28,24 @@ dependencies {
     // TAIL <--
     api(libs.logcat)
 
-    api(libs.rxjava)
+    api(libs.rxJava)
 
     api(libs.okhttp.core)
     api(libs.okhttp.logging)
     api(libs.okhttp.brotli)
-    api(libs.okhttp.dnsoverhttps)
+    api(libs.okhttp.dnsOverHttps)
     api(libs.okio)
 
     implementation(libs.image.decoder)
 
     implementation(libs.unifile)
-    implementation(libs.libarchive)
+    implementation(libs.archive)
 
-    api(kotlinx.coroutines.core)
-    api(kotlinx.serialization.json)
-    api(kotlinx.serialization.json.okio)
+    api(libs.kotlinx.coroutines.core)
+    api(libs.kotlinx.serialization.json)
+    api(libs.kotlinx.serialization.jsonOkio)
 
-    api(libs.preferencektx)
+    api(libs.androidx.preference)
 
     implementation(libs.jsoup)
 
@@ -53,7 +53,7 @@ dependencies {
     implementation(libs.natural.comparator)
 
     // JavaScript engine
-    implementation(libs.bundles.js.engine)
+    implementation(libs.quickJs)
 
     // FFmpeg-kit
     implementation(aniyomilibs.ffmpeg.kit)
