a65485d24af0c98415da30b2fea50b4473f88a90
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-03-25 23:57:35 +0600

    Revert "Update android.gradle to v9 (major) (#3105)"
    This reverts commit 34afdabcfe175bf04e07eb7c567f998c8c173cb6.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,8 +82,8 @@ android {
     }
 
     sourceSets {
-        getByName("preview").res.directories.add("src/debug/res")
-        getByName("benchmark").res.directories.add("src/debug/res")
+        getByName("preview").res.srcDirs("src/debug/res")
+        getByName("benchmark").res.srcDirs("src/debug/res")
     }
 
     splits {
@@ -146,9 +146,9 @@ android {
     buildFeatures {
         viewBinding = true
         buildConfig = true
+        aidl = true
 
         // Disable some unused things
-        aidl = false
         renderScript = false
         shaders = false
     }
