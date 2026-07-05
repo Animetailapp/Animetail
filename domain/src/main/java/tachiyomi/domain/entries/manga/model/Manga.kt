3aefe435d82488c39d3cd9b3c9080651f4e63807
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-07-05 11:51:10 +0600

    Fix crash when putting app in background (#3523)

--- domain/src/main/java/tachiyomi/domain/entries/manga/model/Manga.kt
+++ domain/src/main/java/tachiyomi/domain/entries/manga/model/Manga.kt
@@ -1,16 +1,22 @@
 package tachiyomi.domain.entries.manga.model
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Immutable
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import mihon.core.common.extensions.EMPTY
 import tachiyomi.core.common.preference.TriState
 import tachiyomi.domain.entries.manga.interactor.GetCustomMangaInfo
 import uy.kohesive.injekt.injectLazy
-import java.io.Serializable
+import java.io.ObjectStreamException
 import java.time.Instant
+import java.io.Serializable as JavaSerializable
 
+@SuppressLint("UnsafeOptInUsageError")
+@Serializable
 @Immutable
 data class Manga(
     val id: Long,
@@ -40,7 +46,7 @@ data class Manga(
     val version: Long,
     val notes: String,
     val memo: JsonObject,
-) : Serializable {
+) : JavaSerializable {
 
     // SY -->
     private val customMangaInfo = if (favorite) {
@@ -171,5 +177,18 @@ data class Manga(
         // SY -->
         private val getCustomMangaInfo: GetCustomMangaInfo by injectLazy()
         // SY <--
+    }
+
+    @Throws(ObjectStreamException::class)
+    private fun writeReplace(): Any {
+        return JavaToKotlinXSerializable(Json.encodeToString<Manga>(this))
+    }
+
+    class JavaToKotlinXSerializable(private val data: String) : JavaSerializable {
+
+        @Throws(ObjectStreamException::class)
+        private fun readResolve(): Any {
+            return Json.decodeFromString<Manga>(data)
+        }
     }
 }
