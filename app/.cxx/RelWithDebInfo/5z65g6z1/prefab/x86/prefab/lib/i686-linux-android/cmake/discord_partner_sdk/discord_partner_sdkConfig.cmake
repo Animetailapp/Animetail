b4053b09a598debee9631813ff5f051b2a38b8a5
dark25 <nadiecaca2000@gmail.com>
2026-07-07 21:25:53 +0200

    update

--- app/.cxx/RelWithDebInfo/5z65g6z1/prefab/x86/prefab/lib/i686-linux-android/cmake/discord_partner_sdk/discord_partner_sdkConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET discord_partner_sdk::discord_partner_sdk)
-add_library(discord_partner_sdk::discord_partner_sdk SHARED IMPORTED)
-set_target_properties(discord_partner_sdk::discord_partner_sdk PROPERTIES
-    IMPORTED_LOCATION "C:/Users/amaur/.gradle/caches/9.5.1/transforms/51caf4f0bbded7d8012d9d33f3590ba7/transformed/discord_partner_sdk/prefab/modules/discord_partner_sdk/libs/android.x86/libdiscord_partner_sdk.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/amaur/.gradle/caches/9.5.1/transforms/51caf4f0bbded7d8012d9d33f3590ba7/transformed/discord_partner_sdk/prefab/modules/discord_partner_sdk/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
