pa2x2/katari

Add MangaBaka tracker support (#3047)
* Add support for MangaBaka

Note: missing a client ID which should be added by the project owner.

* Fix typo causing token refresh to fail

The field is called `refresh_token` not `refreshToken`, so all
token refreshes would fail with a 401.

* Use user-preferred score type

Currently, this only set once on login. When selecting a different
score type on MangaBaka, users (currently) will have to log out and
log in again in order for Mihon to refetch the score setting.

This appears to be what Mihon's AniList implementation does as well,
so I followed that.

* Fix score preference

Didn't get caught during the rebase.

* Fix finish_date

* Fix dates appearing offset

Timezone shenanigans meant Mihon was sending an ISO 8601 string for
the UTC timezone which lead to issues in non-UTC timezones.

For example:
1. User in UTC+1 selects 2026-03-22
2. Date picker returns 2026-03-22T00:00:00+01:00
3. MangaBakaApi converts that to an instant, converting it to UTC:
  2026-03-21T23:00:00Z
4. MangaBaka receives that and sets all time components to 0
5. Mihon (when opening the track sheet next) receives:
  2026-03-21T00:00:00Z
6. Mihon renders that as 2026-03-21

I figured out that MangaBaka accepts date-only strings as well, so we
just send those now.

* Remove fetching of total_chapters from MangaBaka

Currently, this information is not what Mihon needs for automatic
marking as COMPLETED since MangaBaka considers this an ongoing tally
of volume-published chapters rather than the final chapter number of a
title.

If anyone other than me used this code: Probably will have to unlink
& relink MB entries to get rid of the total_chapters data, not sure.

MangaBaka is currently considering a request from me to potentially
change this, but currently it's not what we need.

* Fix start date being reset on each read chapter

Admittedly a silly oversight. The other date-supporting trackers (AL,
MAL, Kitsu) use this check as opposed to `>= 0.0`.

* A

app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBaka.kt
@@ -1,0 +1,196 @@
+package eu.kanade.tachiyomi.data.track.mangabaka
+
+import dev.icerock.moko.resources.StringResource
+import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.database.models.Track
+import eu.kanade.tachiyomi.data.track.BaseTracker
+import eu.kanade.tachiyomi.data.track.DeletableTracker
+import eu.kanade.tachiyomi.data.track.mangabaka.dto.MangaBakaOAuth
+import eu.kanade.tachiyomi.data.track.model.TrackSearch
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.serialization.json.Json
+import tachiyomi.i18n.MR
+import uy.kohesive.injekt.injectLazy
+import tachiyomi.domain.track.model.Track as DomainTrack
+
+class MangaBaka(id: Long) : BaseTracker(id, "MangaBaka"), DeletableTracker {
+
+    private val json: Json by injectLazy()
+
+    private val interceptor by lazy { MangaBakaInterceptor(this) }
+    private val api by lazy { MangaBakaApi(id, client, interceptor) }
+
+    override val supportsReadingDates: Boolean = true
+    override val supportsPrivateTracking: Boolean = true
+
+    private val scorePreference = trackPreferences.mangabakaScoreType
+
+    override fun getLogo(): Int = R.drawable.brand_mangabaka
+
+    override fun getStatusList(): List<Long> {
+        return listOf(READING, COMPLETED, PAUSED, DROPPED, PLAN_TO_READ, REREADING, CONSIDERING)
+    }
+
+    override fun getStatus(status: Long): StringResource? = when (status) {
+        CONSIDERING -> MR.strings.considering
+        COMPLETED -> MR.strings.completed
+        DROPPED -> MR.strings.dropped
+        PAUSED -> MR.strings.paused
+        PLAN_TO_READ -> MR.strings.plan_to_read
+        READING -> MR.strings.reading
+        REREADING -> MR.strings.repeating
+        else -> null
+    }
+
+    override fun getReadingStatus(): Long = READING
+
+    override fun getRereadingStatus(): Long = REREADING
+
+    override fun getCompletionStatus(): Long = COMPLETED
+
+    override fun getScoreList(): ImmutableList<String> {
+        return when (scorePreference.get()) {
+            // 1, 2, ..., 99, 100
+            STEP_1 -> IntRange(0, 100).map(Int::toString).toImmutableList()
+            // 5, 10, ..., 95, 100
+            STEP_5 -> IntRange(0, 100).step(5).map(Int::toString).toImmutableList()
+            // 10, 20, ..., 90, 100
+            STEP_10 -> IntRange(0, 100).step(10).map(Int::toString).toImmutableList()
+            // 20, 40, ..., 80, 100
+            STEP_20 -> IntRange(0, 100).step(20).map(Int::toString).toImmutableList()
+            // 25, 50, 75, 100
+            STEP_25 -> IntRange(0, 100).step(25).map(Int::toString).toImmutableList()
+            else -> throw Exception("Unknown score type")
+        }
+    }
+
+    override fun displayScore(track: DomainTrack): String = track.score.toInt().toString()
+
+    override suspend fun update(
+        track: Track,
+        didReadChapter: Boolean,
+    ): Track {
+        if (track.status != COMPLETED && didReadChapter) {
+            if (track.total_chapters > 0 && track.last_chapter_read.toLong() == track.total_chapters) {
+                track.status = COMPLETED
+                track.finished_reading_date = System.currentTimeMillis()
+            } else if (track.status != REREADING) {
+                track.status = READING
+                if (track.last_chapter_read == 1.0) {
+                    track.started_reading_date = System.currentTimeMillis()
+                }
+            }
+        }
+
+        return api.updateLibManga(track)
+    }
+
+    override suspend fun bind(
+        track: Track,
+        hasReadChapters: Boolean,
+    ): Track {
+        val remoteTrack = api.findLibManga(track)
+        return if (remoteTrack != null) {
+            track.copyPersonalFrom(remoteTrack, copyRemotePrivate = false)
+            track.title = remoteTrack.title
+            track.remote_id = remoteTrack.remote_id
+
+            if (track.status != COMPLETED) {
+                val isRereading = track.status == REREADING
+                track.status = if (!isRereading && hasReadChapters) READING else track.status
+            }
+
+            update(track)
+        } else {
+            // Set default fields if it's not found in the list
+            track.status = if (hasReadChapters) READING else PLAN_TO_READ
+            track.score = 0.0
+
+            api.addLibManga(track)
+        }
+    }
+
+    override suspend fun search(query: String): List<TrackSearch> {
+        if (query.startsWith(SEARCH_ID_PREFIX)) {
+            query.substringAfter(SEARCH_ID_PREFIX).toIntOrNull()?.let { id ->
+                return api.getMangaDetails(id)?.let { listOf(it) } ?: emptyList()
+            }
+        }
+
+        return api.search(query)
+    }
+
+    override suspend fun refresh(track: Track): Track {
+        val remoteTrack = api.findLibManga(track) ?: throw Exception("Could not find manga")
+        track.copyPersonalFrom(remoteTrack)
+        track.remote_id = remoteTrack.remote_id
+        track.title = remoteTrack.title
+        return track
+    }
+
+    override suspend fun login(username: String, password: String) = login(password)
+
+    suspend fun login(code: String) {
+        try {
+            val oauth = api.getAccessToken(code)
+            interceptor.setAuth(oauth)
+            val currentUser = api.getCurrentUser()
+            val scoreType = when (currentUser.ratingSteps) {
+                1 -> STEP_1
+                5 -> STEP_5
+                10 -> STEP_10
+                20 -> STEP_20
+                25 -> STEP_25
+                else -> throw Exception("Unknown score step size ${currentUser.ratingSteps}")
+            }
+            scorePreference.set(scoreType)
+            saveDisplayUsername(currentUser.nickname ?: currentUser.preferredUsername ?: currentUser.id)
+            saveCredentials("user", oauth.accessToken)
+        } catch (_: Exception) {
+            logout()
+        }
+    }
+
+    fun saveToken(oauth: MangaBakaOAuth?) {
+        trackPreferences.trackToken(this).set(json.encodeToString(oauth))
+    }
+
+    fun restoreToken(): MangaBakaOAuth? {
+        return try {
+            json.decodeFromString(trackPreferences.trackToken(this).get())
+        } catch (_: Exception) {
+            null
+        }
+    }
+
+    fun verifyOAuthState(state: String): Boolean = api.verifyOAuthState(state)
+
+    override fun logout() {
+        super.logout()
+        trackPreferences.trackToken(this).delete()
+        interceptor.setAuth(null)
+    }
+
+    override suspend fun delete(track: DomainTrack) {
+        api.deleteLibManga(track)
+    }
+
+    companion object {
+        const val READING = 1L
+        const val COMPLETED = 2L
+        const val PAUSED = 3L
+        const val DROPPED = 4L
+        const val PLAN_TO_READ = 5L
+        const val REREADING = 6L
+        const val CONSIDERING = 7L
+
+        const val STEP_1 = "STEP_1"
+        const val STEP_5 = "STEP_5"
+        const val STEP_10 = "STEP_10"
+        const val STEP_20 = "STEP_20"
+        const val STEP_25 = "STEP_25"
+
+        private const val SEARCH_ID_PREFIX = "id:"
+    }
+}

Animetailapp/Animetail

app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBaka.kt
@@ -1,0 +1,214 @@
+package eu.kanade.tachiyomi.data.track.mangabaka
+
+import android.graphics.Color
+import dev.icerock.moko.resources.StringResource
+import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
+import eu.kanade.tachiyomi.data.track.BaseTracker
+import eu.kanade.tachiyomi.data.track.DeletableMangaTracker
+import eu.kanade.tachiyomi.data.track.MangaTracker
+import eu.kanade.tachiyomi.data.track.mangabaka.dto.MangaBakaOAuth
+import eu.kanade.tachiyomi.data.track.model.MangaTrackSearch
+import eu.kanade.tachiyomi.data.track.model.TrackAnimeMetadata
+import eu.kanade.tachiyomi.data.track.model.TrackMangaMetadata
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.serialization.json.Json
+import tachiyomi.i18n.MR
+import uy.kohesive.injekt.injectLazy
+import tachiyomi.domain.track.anime.model.AnimeTrack as DomainAnimeTrack
+import tachiyomi.domain.track.manga.model.MangaTrack as DomainTrack
+
+class MangaBaka(id: Long) :
+    BaseTracker(id, "MangaBaka"),
+    MangaTracker,
+    DeletableMangaTracker {
+
+    private val json: Json by injectLazy()
+
+    private val interceptor by lazy { MangaBakaInterceptor(this) }
+    private val api by lazy { MangaBakaApi(id, client, interceptor) }
+
+    override val supportsReadingDates: Boolean = true
+    override val supportsPrivateTracking: Boolean = true
+
+    private val scorePreference = trackPreferences.mangabakaScoreType
+
+    override fun getLogo(): Int = R.drawable.brand_mangabaka
+
+    override fun getLogoColor(): Int = Color.rgb(222, 59, 64)
+
+    override fun getStatusListManga(): List<Long> {
+        return listOf(READING, COMPLETED, PAUSED, DROPPED, PLAN_TO_READ, REREADING, CONSIDERING)
+    }
+
+    override fun getStatusForManga(status: Long): StringResource? = when (status) {
+        CONSIDERING -> MR.strings.considering
+        COMPLETED -> MR.strings.completed
+        DROPPED -> MR.strings.dropped
+        PAUSED -> MR.strings.paused
+        PLAN_TO_READ -> MR.strings.plan_to_read
+        READING -> MR.strings.reading
+        REREADING -> MR.strings.repeating
+        else -> null
+    }
+
+    override fun getReadingStatus(): Long = READING
+
+    override fun getRereadingStatus(): Long = REREADING
+
+    override fun getCompletionStatus(): Long = COMPLETED
+
+    override fun getScoreList(): ImmutableList<String> {
+        return when (scorePreference.get()) {
+            // 1, 2, ..., 99, 100
+            STEP_1 -> IntRange(0, 100).map(Int::toString).toImmutableList()
+            // 5, 10, ..., 95, 100
+            STEP_5 -> IntRange(0, 100).step(5).map(Int::toString).toImmutableList()
+            // 10, 20, ..., 90, 100
+            STEP_10 -> IntRange(0, 100).step(10).map(Int::toString).toImmutableList()
+            // 20, 40, ..., 80, 100
+            STEP_20 -> IntRange(0, 100).step(20).map(Int::toString).toImmutableList()
+            // 25, 50, 75, 100
+            STEP_25 -> IntRange(0, 100).step(25).map(Int::toString).toImmutableList()
+            else -> throw Exception("Unknown score type")
+        }
+    }
+
+    override fun displayScore(track: DomainTrack): String = track.score.toInt().toString()
+
+    override suspend fun update(
+        track: MangaTrack,
+        didReadChapter: Boolean,
+    ): MangaTrack {
+        if (track.status != COMPLETED && didReadChapter) {
+            if (track.total_chapters > 0 && track.last_chapter_read.toLong() == track.total_chapters) {
+                track.status = COMPLETED
+                track.finished_reading_date = System.currentTimeMillis()
+            } else if (track.status != REREADING) {
+                track.status = READING
+                if (track.last_chapter_read == 1.0) {
+                    track.started_reading_date = System.currentTimeMillis()
+                }
+            }
+        }
+
+        return api.updateLibManga(track)
+    }
+
+    override suspend fun bind(
+        track: MangaTrack,
+        hasReadChapters: Boolean,
+    ): MangaTrack {
+        val remoteTrack = api.findLibManga(track)
+        return if (remoteTrack != null) {
+            track.copyPersonalFrom(remoteTrack, copyRemotePrivate = false)
+            track.title = remoteTrack.title
+            track.remote_id = remoteTrack.remote_id
+
+            if (track.status != COMPLETED) {
+                val isRereading = track.status == REREADING
+                track.status = if (!isRereading && hasReadChapters) READING else track.status
+            }
+
+            update(track)
+        } else {
+            // Set default fields if it's not found in the list
+            track.status = if (hasReadChapters) READING else PLAN_TO_READ
+            track.score = 0.0
+
+            api.addLibManga(track)
+        }
+    }
+
+    override suspend fun searchManga(query: String): List<MangaTrackSearch> {
+        if (query.startsWith(SEARCH_ID_PREFIX)) {
+            query.substringAfter(SEARCH_ID_PREFIX).toIntOrNull()?.let { id ->
+                return api.getMangaDetails(id)?.let { listOf(it) } ?: emptyList()
+            }
+        }
+
+        return api.search(query)
+    }
+
+    override suspend fun refresh(track: MangaTrack): MangaTrack {
+        val remoteTrack = api.findLibManga(track) ?: throw Exception("Could not find manga")
+        track.copyPersonalFrom(remoteTrack)
+        track.remote_id = remoteTrack.remote_id
+        track.title = remoteTrack.title
+        return track
+    }
+
+    override suspend fun login(username: String, password: String) = login(password)
+
+    suspend fun login(code: String) {
+        try {
+            val oauth = api.getAccessToken(code)
+            interceptor.setAuth(oauth)
+            val currentUser = api.getCurrentUser()
+            val scoreType = when (currentUser.ratingSteps) {
+                1 -> STEP_1
+                5 -> STEP_5
+                10 -> STEP_10
+                20 -> STEP_20
+                25 -> STEP_25
+                else -> throw Exception("Unknown score step size ${currentUser.ratingSteps}")
+            }
+            scorePreference.set(scoreType)
+            saveDisplayUsername(currentUser.nickname ?: currentUser.preferredUsername ?: currentUser.id)
+            saveCredentials("user", oauth.accessToken)
+        } catch (_: Exception) {
+            logout()
+        }
+    }
+
+    fun saveToken(oauth: MangaBakaOAuth?) {
+        trackPreferences.trackToken(this).set(json.encodeToString(oauth))
+    }
+
+    fun restoreToken(): MangaBakaOAuth? {
+        return try {
+            json.decodeFromString(trackPreferences.trackToken(this).get())
+        } catch (_: Exception) {
+            null
+        }
+    }
+
+    fun verifyOAuthState(state: String): Boolean = api.verifyOAuthState(state)
+
+    override fun logout() {
+        super.logout()
+        trackPreferences.trackToken(this).delete()
+        interceptor.setAuth(null)
+    }
+
+    override suspend fun delete(track: DomainTrack) {
+        api.deleteLibManga(track)
+    }
+
+    override suspend fun getMangaMetadata(track: DomainTrack): TrackMangaMetadata? {
+        return null
+    }
+
+    override suspend fun getAnimeMetadata(track: DomainAnimeTrack): TrackAnimeMetadata? {
+        return null
+    }
+
+    companion object {
+        const val READING = 1L
+        const val COMPLETED = 2L
+        const val PAUSED = 3L
+        const val DROPPED = 4L
+        const val PLAN_TO_READ = 5L
+        const val REREADING = 6L
+        const val CONSIDERING = 7L
+
+        const val STEP_1 = "STEP_1"
+        const val STEP_5 = "STEP_5"
+        const val STEP_10 = "STEP_10"
+        const val STEP_20 = "STEP_20"
+        const val STEP_25 = "STEP_25"
+
+        private const val SEARCH_ID_PREFIX = "id:"
+    }
+}