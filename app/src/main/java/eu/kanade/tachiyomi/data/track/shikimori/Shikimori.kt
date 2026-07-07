ouassim-boulkamh/amatsubu

Use Shikimori's GraphQL API where possible (#3499)
User list mutation aren't available via GraphQL, so that will still be
done with the v2 API.

Shikimori API docs say to prefer the GraphQL API when possible:
https://shikimori.io/api/doc

Allows adding some additional data in the search results, namely:
- Authors & Artists
- Description

As a nice bonus, this reduces the number of requests to Shikimori
because the findLibManga method no longer needs to do 2 calls to fetch
both list and title data.

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/Shikimori.kt
@@ -61,7 +61,7 @@ class Shikimori(id: Long) : BaseTracker(id, "Shikimori"), DeletableTracker {
     }
 
     override suspend fun bind(track: Track, hasReadChapters: Boolean): Track {
-        val remoteTrack = api.findLibManga(track, getUsername())
+        val remoteTrack = api.findLibManga(track)
         return if (remoteTrack != null) {
             track.copyPersonalFrom(remoteTrack)
             track.library_id = remoteTrack.library_id
@@ -85,7 +85,7 @@ class Shikimori(id: Long) : BaseTracker(id, "Shikimori"), DeletableTracker {
     }
 
     override suspend fun refresh(track: Track): Track {
-        api.findLibManga(track, getUsername())?.let { remoteTrack ->
+        api.findLibManga(track, isRefresh = true)?.let { remoteTrack ->
             track.library_id = remoteTrack.library_id
             track.copyPersonalFrom(remoteTrack)
             track.total_chapters = remoteTrack.total_chapters

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/dto/SMUserListEntry.kt
@@ -1,27 +1,52 @@
 package eu.kanade.tachiyomi.data.track.shikimori.dto
 
 import eu.kanade.tachiyomi.data.database.models.Track
-import eu.kanade.tachiyomi.data.track.shikimori.ShikimoriApi
 import eu.kanade.tachiyomi.data.track.shikimori.toTrackStatus
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class SMUserListEntry(
-    val id: Long,
-    val chapters: Double,
-    val score: Int,
-    val status: String,
+data class SMUserListResult(
+    val data: SMUserListEntries,
+)
+
+@Serializable
+data class SMUserListEntries(
+    val mangas: List<SMUserListManga>,
+)
+
+@Serializable
+data class SMUserListManga(
+    val id: String,
+    val url: String,
+    val name: String,
+    @SerialName("chapters")
+    val totalChapters: Long, // the title's total chapters
+    val userRate: SMUserRate?,
 ) {
-    fun toTrack(trackId: Long, manga: SMManga): Track {
+    fun toTrack(trackId: Long): Track {
         return Track.create(trackId).apply {
-            title = manga.name
-            remote_id = this@SMUserListEntry.id
-            total_chapters = manga.chapters
-            library_id = this@SMUserListEntry.id
-            last_chapter_read = this@SMUserListEntry.chapters
-            score = this@SMUserListEntry.score.toDouble()
-            status = toTrackStatus(this@SMUserListEntry.status)
-            tracking_url = ShikimoriApi.BASE_URL + manga.url
+            title = name
+            total_chapters = totalChapters
+            tracking_url = url
+            if (userRate != null) {
+                // null if not in user's list, must not throw here because it'd break adding titles
+                // throws in the findLibManga method of ShikimoriApi if null and shouldn't be
+                remote_id = userRate.rateId.toLong()
+                library_id = userRate.rateId.toLong()
+                last_chapter_read = userRate.chapters.toDouble()
+                score = userRate.score
+                status = toTrackStatus(userRate.status)
+            }
         }
     }
 }
+
+@Serializable
+data class SMUserRate(
+    @SerialName("id")
+    val rateId: String, // ID of the list entry (NOT the title)
+    val chapters: Long, // the user's chapter progress
+    val status: String,
+    val score: Double,
+)

Animetailapp/Animetail

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/Shikimori.kt
@@ -129,7 +129,7 @@ class Shikimori(id: Long) :
     }
 
     override suspend fun bind(track: AnimeTrack, hasSeenEpisodes: Boolean): AnimeTrack {
-        val remoteTrack = api.findLibAnime(track, getUsername())
+        val remoteTrack = api.findLibAnime(track)
         return if (remoteTrack != null) {
             track.copyPersonalFrom(remoteTrack)
             track.library_id = remoteTrack.library_id
@@ -166,7 +166,7 @@ class Shikimori(id: Long) :
     }
 
     override suspend fun refresh(track: AnimeTrack): AnimeTrack {
-        api.findLibAnime(track, getUsername())?.let { remoteTrack ->
+        api.findLibAnime(track, isRefresh = true)?.let { remoteTrack ->
             track.library_id = remoteTrack.library_id
             track.copyPersonalFrom(remoteTrack)
             track.total_episodes = remoteTrack.total_episodes
@@ -177,6 +177,7 @@ class Shikimori(id: Long) :
     override suspend fun getMangaMetadata(track: DomainMangaTrack): TrackMangaMetadata? {
         return api.getMangaMetadata(track)
     }
+
     override suspend fun getAnimeMetadata(track: DomainAnimeTrack): TrackAnimeMetadata? {
         return api.getAnimeMetadata(track)
     }

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/dto/SMUserListEntry.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.data.track.shikimori.dto
 
 import eu.kanade.tachiyomi.data.database.models.anime.AnimeTrack
 import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
-import eu.kanade.tachiyomi.data.track.shikimori.ShikimoriApi
 import eu.kanade.tachiyomi.data.track.shikimori.toTrackStatus
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -14,33 +13,7 @@ data class SMUserListEntry(
     val episodes: Double,
     val score: Int,
     val status: String,
-) {
-    fun toMangaTrack(trackId: Long, manga: SMEntry): MangaTrack {
-        return MangaTrack.create(trackId).apply {
-            title = manga.name
-            remote_id = this@SMUserListEntry.id
-            total_chapters = manga.chapters!!
-            library_id = this@SMUserListEntry.id
-            last_chapter_read = this@SMUserListEntry.chapters
-            score = this@SMUserListEntry.score.toDouble()
-            status = toTrackStatus(this@SMUserListEntry.status)
-            tracking_url = ShikimoriApi.BASE_URL + manga.url
-        }
-    }
-
-    fun toAnimeTrack(trackId: Long, anime: SMEntry): AnimeTrack {
-        return AnimeTrack.create(trackId).apply {
-            title = anime.name
-            remote_id = this@SMUserListEntry.id
-            total_episodes = anime.episodes!!
-            library_id = this@SMUserListEntry.id
-            last_episode_seen = this@SMUserListEntry.episodes
-            score = this@SMUserListEntry.score.toDouble()
-            status = toTrackStatus(this@SMUserListEntry.status)
-            tracking_url = ShikimoriApi.BASE_URL + anime.url
-        }
-    }
-}
+)
 
 @Serializable
 data class SMUserListResult(
@@ -58,7 +31,7 @@ data class SMUserListManga(
     val url: String,
     val name: String,
     @SerialName("chapters")
-    val totalChapters: Long, // the title's total chapters
+    val totalChapters: Long,
     val userRate: SMUserRate?,
 ) {
     fun toTrack(trackId: Long): MangaTrack {
@@ -67,8 +40,6 @@ data class SMUserListManga(
             total_chapters = totalChapters
             tracking_url = url
             if (userRate != null) {
-                // null if not in user's list, must not throw here because it'd break adding titles
-                // throws in the findLibManga method of ShikimoriApi if null and shouldn't be
                 remote_id = userRate.rateId.toLong()
                 library_id = userRate.rateId.toLong()
                 last_chapter_read = userRate.chapters.toDouble()
@@ -80,10 +51,54 @@ data class SMUserListManga(
 }
 
 @Serializable
+data class SMAnimeUserListResult(
+    val data: SMAnimeUserListEntries,
+)
+
+@Serializable
+data class SMAnimeUserListEntries(
+    val animes: List<SMAnimeUserListAnime>,
+)
+
+@Serializable
+data class SMAnimeUserListAnime(
+    val id: String,
+    val url: String,
+    val name: String,
+    @SerialName("episodes")
+    val totalEpisodes: Long,
+    val userRate: SMAnimeUserRate?,
+) {
+    fun toTrack(trackId: Long): AnimeTrack {
+        return AnimeTrack.create(trackId).apply {
+            title = name
+            total_episodes = totalEpisodes
+            tracking_url = url
+            if (userRate != null) {
+                remote_id = userRate.rateId.toLong()
+                library_id = userRate.rateId.toLong()
+                last_episode_seen = userRate.episodes.toDouble()
+                score = userRate.score
+                status = toTrackStatus(userRate.status)
+            }
+        }
+    }
+}
+
+@Serializable
 data class SMUserRate(
     @SerialName("id")
-    val rateId: String, // ID of the list entry (NOT the title)
-    val chapters: Long, // the user's chapter progress
+    val rateId: String,
+    val chapters: Long,
+    val status: String,
+    val score: Double,
+)
+
+@Serializable
+data class SMAnimeUserRate(
+    @SerialName("id")
+    val rateId: String,
+    val episodes: Long,
     val status: String,
     val score: Double,
 )