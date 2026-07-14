1e94c7957fdb790addd434b797fcb56599e6ac1b
Dark25 <nadiecaca2000@gmail.com>
2026-07-14 21:41:33 +0200

    fix(torrent): restore binary compatibility for extension torrent utils API (#440)

--- source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/torrentutils/TorrentUtils.kt
+++ source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/torrentutils/TorrentUtils.kt
@@ -5,18 +5,19 @@ import aniyomi.core.common.torrent.TorrentServerApi
 import eu.kanade.tachiyomi.torrentutils.model.DeadTorrentException
 import eu.kanade.tachiyomi.torrentutils.model.TorrentFile
 import eu.kanade.tachiyomi.torrentutils.model.TorrentInfo
+import kotlinx.coroutines.runBlocking
 import uy.kohesive.injekt.injectLazy
 import java.net.SocketTimeoutException
 
 object TorrentUtils {
     private val torrentServerApi: TorrentServerApi by injectLazy()
 
-    suspend fun getTorrentInfo(
+    fun getTorrentInfo(
         url: String,
         title: String,
     ): TorrentInfo {
         try {
-            val torrent = torrentServerApi.addTorrent(url, title, "", "", false)
+            val torrent = runBlocking { torrentServerApi.addTorrent(url, title, "", "", false) }
             return TorrentInfo(
                 torrent.title,
                 torrent.fileStats?.map { file ->
