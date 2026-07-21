pa2x2/katari

Migrate to AndroidX ViewModel from Voyager ScreenModel (#3594)

app/src/main/java/eu/kanade/tachiyomi/ui/browse/BrowseTab.kt
@@ -9,14 +9,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
-import cafe.adriel.voyager.core.model.rememberScreenModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import eu.kanade.presentation.components.TabbedScreen
 import eu.kanade.presentation.util.Tab
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.ui.browse.extension.ExtensionsScreenModel
+import eu.kanade.tachiyomi.ui.browse.extension.ExtensionsViewModel
 import eu.kanade.tachiyomi.ui.browse.extension.extensionsTab
 import eu.kanade.tachiyomi.ui.browse.migration.sources.migrateSourceTab
 import eu.kanade.tachiyomi.ui.browse.source.globalsearch.GlobalSearchScreen
@@ -58,12 +58,12 @@ data object BrowseTab : Tab {
         val context = LocalContext.current
 
         // Hoisted for extensions tab's search bar
-        val extensionsScreenModel = rememberScreenModel { ExtensionsScreenModel() }
-        val extensionsState by extensionsScreenModel.state.collectAsState()
+        val extensionsViewModel = viewModel<ExtensionsViewModel>()
+        val extensionsState by extensionsViewModel.state.collectAsState()
 
         val tabs = listOf(
             sourcesTab(),
-            extensionsTab(extensionsScreenModel),
+            extensionsTab(extensionsViewModel),
             migrateSourceTab(),
         )
 
@@ -74,7 +74,7 @@ data object BrowseTab : Tab {
             tabs = tabs,
             state = state,
             searchQuery = extensionsState.searchQuery,
-            onChangeSearchQuery = extensionsScreenModel::search,
+            onChangeSearchQuery = extensionsViewModel::search,
         )
         LaunchedEffect(Unit) {
             switchToExtensionTabChannel.receiveAsFlow()

app/src/main/java/eu/kanade/tachiyomi/ui/manga/notes/MangaNotesScreen.kt
@@ -4,14 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import cafe.adriel.voyager.core.model.StateScreenModel
-import cafe.adriel.voyager.core.model.rememberScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import eu.kanade.presentation.manga.MangaNotesScreen
 import eu.kanade.presentation.util.Screen
 import kotlinx.coroutines.flow.update
+import mihon.core.viewmodel.StateViewModel
 import tachiyomi.core.common.util.lang.launchNonCancellable
 import tachiyomi.domain.manga.interactor.UpdateMangaNotes
 import tachiyomi.domain.manga.model.Manga
@@ -25,20 +28,37 @@ class MangaNotesScreen(
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
 
-        val screenModel = rememberScreenModel { Model(manga) }
-        val state by screenModel.state.collectAsState()
+        val viewModel = viewModel<Model>(
+            factory = Model.Factory,
+            extras = CreationExtras {
+                set(Model.MANGA_KEY, manga)
+            },
+        )
+        val state by viewModel.state.collectAsState()
 
         MangaNotesScreen(
             state = state,
             navigateUp = navigator::pop,
-            onUpdate = screenModel::updateNotes,
+            onUpdate = viewModel::updateNotes,
         )
     }
 
     private class Model(
         private val manga: Manga,
         private val updateMangaNotes: UpdateMangaNotes = Injekt.get(),
-    ) : StateScreenModel<State>(State(manga, manga.notes)) {
+    ) : StateViewModel<State>(State(manga, manga.notes)) {
+
+        companion object {
+            val MANGA_KEY = CreationExtras.Key<Manga>()
+
+            val Factory = viewModelFactory {
+                initializer {
+                    Model(
+                        manga = get(MANGA_KEY)!!,
+                    )
+                }
+            }
+        }
 
         fun updateNotes(content: String) {
             if (content == state.value.notes) return
@@ -47,7 +67,7 @@ class MangaNotesScreen(
                 it.copy(notes = content)
             }
 
-            screenModelScope.launchNonCancellable {
+            viewModelScope.launchNonCancellable {
                 updateMangaNotes(manga.id, content)
             }
         }

Animetailapp/Animetail

app/src/main/java/eu/kanade/tachiyomi/ui/browse/BrowseTab.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
@@ -29,7 +30,7 @@ import eu.kanade.tachiyomi.ui.browse.anime.source.animeSourcesTab
 import eu.kanade.tachiyomi.ui.browse.anime.source.globalsearch.GlobalAnimeSearchScreen
 import eu.kanade.tachiyomi.ui.browse.feed.FeedScreenModel
 import eu.kanade.tachiyomi.ui.browse.feed.feedTab
-import eu.kanade.tachiyomi.ui.browse.manga.extension.MangaExtensionsScreenModel
+import eu.kanade.tachiyomi.ui.browse.manga.extension.MangaExtensionsViewModel
 import eu.kanade.tachiyomi.ui.browse.manga.extension.mangaExtensionsTab
 import eu.kanade.tachiyomi.ui.browse.manga.migration.sources.migrateMangaSourceTab
 import eu.kanade.tachiyomi.ui.browse.manga.source.mangaSourcesTab
@@ -88,14 +89,14 @@ data object BrowseTab : Tab {
         // SY <--
 
         // Hoisted for extensions tab's search bar
-        val mangaExtensionsScreenModel = rememberScreenModel { MangaExtensionsScreenModel() }
-        val mangaExtensionsState by mangaExtensionsScreenModel.state.collectAsState()
+        val mangaExtensionsViewModel = viewModel<MangaExtensionsViewModel>()
+        val mangaExtensionsState by mangaExtensionsViewModel.state.collectAsState()
 
         val animeExtensionsScreenModel = rememberScreenModel { AnimeExtensionsScreenModel() }
         val animeExtensionsState by animeExtensionsScreenModel.state.collectAsState()
 
         val animeExtensionsTabContent = animeExtensionsTab(animeExtensionsScreenModel)
-        val mangaExtensionsTabContent = mangaExtensionsTab(mangaExtensionsScreenModel)
+        val mangaExtensionsTabContent = mangaExtensionsTab(mangaExtensionsViewModel)
 
         // KMK -->
         val feedScreenModel = rememberScreenModel { FeedScreenModel() }
@@ -158,7 +159,7 @@ data object BrowseTab : Tab {
             tabs = tabs,
             state = state,
             mangaSearchQuery = mangaExtensionsState.searchQuery,
-            onChangeMangaSearchQuery = mangaExtensionsScreenModel::search,
+            onChangeMangaSearchQuery = mangaExtensionsViewModel::search,
             animeSearchQuery = animeExtensionsState.searchQuery,
             onChangeAnimeSearchQuery = animeExtensionsScreenModel::search,
             animeExtensionsTabIndex = animeExtensionsTabIndex,
@@ -189,3 +190,4 @@ data object BrowseTab : Tab {
         }
     }
 }
+

app/src/main/java/eu/kanade/tachiyomi/ui/entries/manga/notes/MangaNotesScreen.kt
@@ -4,15 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import cafe.adriel.voyager.core.model.StateScreenModel
-import cafe.adriel.voyager.core.model.rememberScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import eu.kanade.presentation.entries.manga.MangaNotesScreen
 import eu.kanade.presentation.util.Screen
 import kotlinx.coroutines.flow.update
-import kotlinx.serialization.Serializable
+import mihon.core.viewmodel.StateViewModel
 import tachiyomi.core.common.util.lang.launchNonCancellable
 import tachiyomi.domain.entries.manga.interactor.UpdateMangaNotes
 import uy.kohesive.injekt.Injekt
@@ -27,28 +29,45 @@ class MangaNotesScreen(
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
 
-        val screenModel = rememberScreenModel {
-            Model(
-                mangaId = mangaId,
-                mangaTitle = mangaTitle,
-                mangaNotes = mangaNotes,
-            )
-        }
-        val state by screenModel.state.collectAsState()
+        val viewModel = viewModel<Model>(
+            factory = Model.Factory,
+            extras = CreationExtras {
+                set(Model.MANGA_ID_KEY, mangaId)
+                set(Model.MANGA_TITLE_KEY, mangaTitle)
+                set(Model.MANGA_NOTES_KEY, mangaNotes)
+            },
+        )
+        val state by viewModel.state.collectAsState()
 
         MangaNotesScreen(
             state = state,
             navigateUp = navigator::pop,
-            onUpdate = screenModel::updateNotes,
+            onUpdate = viewModel::updateNotes,
         )
     }
 
     private class Model(
+        private val mangaId: Long,
         mangaTitle: String,
         mangaNotes: String,
-        private val mangaId: Long,
         private val updateMangaNotes: UpdateMangaNotes = Injekt.get(),
-    ) : StateScreenModel<State>(State(mangaId, mangaTitle, mangaNotes)) {
+    ) : StateViewModel<State>(State(mangaId, mangaTitle, mangaNotes)) {
+
+        companion object {
+            val MANGA_ID_KEY = CreationExtras.Key<Long>()
+            val MANGA_TITLE_KEY = CreationExtras.Key<String>()
+            val MANGA_NOTES_KEY = CreationExtras.Key<String>()
+
+            val Factory = viewModelFactory {
+                initializer {
+                    Model(
+                        mangaId = get(MANGA_ID_KEY)!!,
+                        mangaTitle = get(MANGA_TITLE_KEY)!!,
+                        mangaNotes = get(MANGA_NOTES_KEY)!!,
+                    )
+                }
+            }
+        }
 
         fun updateNotes(content: String) {
             if (content == state.value.notes) return
@@ -57,7 +76,7 @@ class MangaNotesScreen(
                 it.copy(notes = content)
             }
 
-            screenModelScope.launchNonCancellable {
+            viewModelScope.launchNonCancellable {
                 updateMangaNotes(mangaId, content)
             }
         }