68f4a0a5d2bf75b81d4d384f508c699955ea5846
dark25 <nadiecaca2000@gmail.com>
2026-08-10 14:19:23 +0200

    fix(network): use BrotliInterceptor as application interceptor and remove IgnoreGzipInterceptor

--- core/common/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ core/common/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -3,7 +3,6 @@ package eu.kanade.tachiyomi.network
 import android.content.Context
 import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
 import eu.kanade.tachiyomi.network.interceptor.FlareSolverrInterceptor
-import eu.kanade.tachiyomi.network.interceptor.IgnoreGzipInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UncaughtExceptionInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UserAgentInterceptor
 import kotlinx.coroutines.CoroutineScope
@@ -36,10 +35,9 @@ class NetworkHelper(
                     maxSize = 5L * 1024 * 1024, // 5 MiB
                 ),
             )
+            .addInterceptor(BrotliInterceptor)
             .addInterceptor(UncaughtExceptionInterceptor())
             .addInterceptor(UserAgentInterceptor(::defaultUserAgentProvider))
-            .addNetworkInterceptor(IgnoreGzipInterceptor())
-            .addNetworkInterceptor(BrotliInterceptor)
             // TLMR -->
             .addInterceptor(FlareSolverrInterceptor(preferences))
         // <-- TLMR
