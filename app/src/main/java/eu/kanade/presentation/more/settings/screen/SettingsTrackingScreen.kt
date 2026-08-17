mihonapp/mihon

Migrate to Metro DI (#3608)
Assisted-by: Claude:claude-opus-5

app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsTrackingScreen.kt
@@ -44,11 +44,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.resources.StringResource
 import eu.kanade.domain.track.model.AutoTrackState
-import eu.kanade.domain.track.service.TrackPreferences
 import eu.kanade.presentation.more.settings.Preference
 import eu.kanade.tachiyomi.data.track.EnhancedTracker
 import eu.kanade.tachiyomi.data.track.Tracker
-import eu.kanade.tachiyomi.data.track.TrackerManager
 import eu.kanade.tachiyomi.data.track.anilist.AnilistApi
 import eu.kanade.tachiyomi.data.track.bangumi.BangumiApi
 import eu.kanade.tachiyomi.data.track.hikka.HikkaApi
@@ -57,14 +55,12 @@ import eu.kanade.tachiyomi.data.track.myanimelist.MyAnimeListApi
 import eu.kanade.tachiyomi.data.track.shikimori.ShikimoriApi
 import eu.kanade.tachiyomi.util.system.openInBrowser
 import eu.kanade.tachiyomi.util.system.toast
+import mihon.app.di.appGraph
 import tachiyomi.core.common.util.lang.launchIO
 import tachiyomi.core.common.util.lang.withUIContext
-import tachiyomi.domain.source.service.SourceManager
 import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.components.material.padding
 import tachiyomi.presentation.core.i18n.stringResource
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
 object SettingsTrackingScreen : SearchableSettings {
 
@@ -86,9 +82,9 @@ object SettingsTrackingScreen : SearchableSettings {
     @Composable
     override fun getPreferences(): List<Preference> {
         val context = LocalContext.current
-        val trackPreferences = remember { Injekt.get<TrackPreferences>() }
-        val trackerManager = remember { Injekt.get<TrackerManager>() }
-        val sourceManager = remember { Injekt.get<SourceManager>() }
+        val trackPreferences = remember { context.appGraph.trackPreferences }
+        val trackerManager = remember { context.appGraph.trackerManager }
+        val sourceManager = remember { context.appGraph.sourceManager }
 
         var dialog by remember { mutableStateOf<Any?>(null) }
         dialog?.run {

app/src/main/java/eu/kanade/tachiyomi/ui/updates/UpdatesSettingsViewModel.kt
@@ -1,18 +1,23 @@
 package eu.kanade.tachiyomi.ui.updates
 
 import androidx.lifecycle.ViewModel
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesIntoMap
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metrox.viewmodel.ViewModelKey
 import tachiyomi.core.common.preference.Preference
 import tachiyomi.core.common.preference.TriState
 import tachiyomi.core.common.preference.getAndSet
 import tachiyomi.domain.category.interactor.GetCategories
 import tachiyomi.domain.category.model.Category
 import tachiyomi.domain.updates.service.UpdatesPreferences
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
+@Inject
+@ViewModelKey
+@ContributesIntoMap(AppScope::class)
 class UpdatesSettingsViewModel(
-    val updatesPreferences: UpdatesPreferences = Injekt.get(),
-    val getCategories: GetCategories = Injekt.get(),
+    val updatesPreferences: UpdatesPreferences,
+    val getCategories: GetCategories,
 ) : ViewModel() {
 
     val includedCategories = updatesPreferences.filterIncludedCategories

app/src/main/java/eu/kanade/tachiyomi/util/chapter/ChapterFilterDownloaded.kt
@@ -4,16 +4,12 @@ import eu.kanade.tachiyomi.data.download.DownloadCache
 import tachiyomi.domain.chapter.model.Chapter
 import tachiyomi.domain.manga.model.Manga
 import tachiyomi.source.local.isLocal
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
 /**
  * Returns a copy of the list with not downloaded chapters removed.
  */
-fun List<Chapter>.filterDownloaded(manga: Manga): List<Chapter> {
+fun List<Chapter>.filterDownloaded(manga: Manga, downloadCache: DownloadCache): List<Chapter> {
     if (manga.isLocal()) return this
-
-    val downloadCache: DownloadCache = Injekt.get()
 
     return filter { downloadCache.isChapterDownloaded(it.name, it.scanlator, it.url, manga.title, manga.source, false) }
 }

Animetailapp/Animetail

app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsTrackingScreen.kt
@@ -42,12 +42,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.resources.StringResource
 import eu.kanade.domain.track.model.AutoTrackState
-import eu.kanade.domain.track.service.TrackPreferences
 import eu.kanade.presentation.more.settings.Preference
 import eu.kanade.tachiyomi.data.track.EnhancedAnimeTracker
 import eu.kanade.tachiyomi.data.track.EnhancedMangaTracker
 import eu.kanade.tachiyomi.data.track.Tracker
-import eu.kanade.tachiyomi.data.track.TrackerManager
 import eu.kanade.tachiyomi.data.track.anilist.AnilistApi
 import eu.kanade.tachiyomi.data.track.bangumi.BangumiApi
 import eu.kanade.tachiyomi.data.track.hikka.HikkaApi
@@ -61,6 +59,7 @@ import eu.kanade.tachiyomi.util.system.toast
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.collections.immutable.toPersistentMap
+import mihon.app.di.appGraph
 import tachiyomi.core.common.util.lang.launchIO
 import tachiyomi.core.common.util.lang.withUIContext
 import tachiyomi.domain.source.anime.service.AnimeSourceManager
@@ -70,8 +69,6 @@ import tachiyomi.i18n.aniyomi.AYMR
 import tachiyomi.i18n.tail.TLMR
 import tachiyomi.presentation.core.components.material.padding
 import tachiyomi.presentation.core.i18n.stringResource
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
 object SettingsTrackingScreen : SearchableSettings {
 
@@ -94,10 +91,10 @@ object SettingsTrackingScreen : SearchableSettings {
     override fun getPreferences(): List<Preference> {
         val context = LocalContext.current
         val scope = rememberCoroutineScope()
-        val trackPreferences = remember { Injekt.get<TrackPreferences>() }
-        val trackerManager = remember { Injekt.get<TrackerManager>() }
-        val mangaSourceManager = remember { Injekt.get<MangaSourceManager>() }
-        val animeSourceManager = remember { Injekt.get<AnimeSourceManager>() }
+        val trackPreferences = remember { context.appGraph.trackPreferences }
+        val trackerManager = remember { context.appGraph.trackerManager }
+        val mangaSourceManager = remember { context.appGraph.mangaSourceManager }
+        val animeSourceManager = remember { context.appGraph.animeSourceManager }
 
         var dialog by remember { mutableStateOf<Any?>(null) }
         dialog?.run {
@@ -467,8 +464,8 @@ private fun TrackingApiKeyDialog(
     onDismissRequest: () -> Unit,
 ) {
     val context = LocalContext.current
-    val trackPreferences = remember { Injekt.get<TrackPreferences>() }
-    val networkHelper = remember { Injekt.get<eu.kanade.tachiyomi.network.NetworkHelper>() }
+    val trackPreferences = remember { context.appGraph.trackPreferences }
+    val networkHelper = remember { context.appGraph.networkHelper }
     val scope = rememberCoroutineScope()
 
     var apiKey by remember { mutableStateOf(TextFieldValue(trackPreferences.trackApiKey(tracker).get())) }
@@ -506,11 +503,12 @@ private fun TrackingApiKeyDialog(
                 modifier = Modifier.fillMaxWidth(),
                 enabled = !processing && apiKey.text.isNotBlank(),
                 onClick = {
+                    val keyText = apiKey.text
                     scope.launchIO {
                         processing = true
                         try {
                             // Validate API key by requesting /3/configuration
-                            val url = "https://api.themoviedb.org/3/configuration?api_key=${apiKey.text}"
+                            val url = "https://api.themoviedb.org/3/configuration?api_key=$keyText"
                             val req = okhttp3.Request.Builder().url(url).get().build()
                             val resp = networkHelper.client.newCall(req).execute()
                             val ok = try {
@@ -520,7 +518,7 @@ private fun TrackingApiKeyDialog(
                             }
 
                             if (ok) {
-                                trackPreferences.setApiKey(tracker, apiKey.text)
+                                trackPreferences.setApiKey(tracker, keyText)
                                 withUIContext {
                                     onDismissRequest()
                                     context.toast(MR.strings.login_success)
@@ -536,8 +534,7 @@ private fun TrackingApiKeyDialog(
                     }
                 },
             ) {
-                val id = if (processing) MR.strings.loading else TLMR.strings.save
-                Text(text = stringResource(id))
+                Text(text = if (processing) stringResource(MR.strings.loading) else stringResource(TLMR.strings.save))
             }
         },
         dismissButton = {

app/src/main/java/eu/kanade/tachiyomi/ui/updates/UpdatesSettingsViewModel.kt
@@ -1,18 +1,24 @@
 package eu.kanade.tachiyomi.ui.updates
 
 import androidx.lifecycle.ViewModel
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesIntoMap
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.binding
+import dev.zacsweers.metrox.viewmodel.ViewModelKey
 import tachiyomi.core.common.preference.Preference
 import tachiyomi.core.common.preference.TriState
 import tachiyomi.core.common.preference.getAndSet
 import tachiyomi.domain.category.manga.interactor.GetMangaCategories
 import tachiyomi.domain.category.model.Category
 import tachiyomi.domain.updates.service.UpdatesPreferences
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
+@Inject
+@ViewModelKey
+@ContributesIntoMap(AppScope::class, binding = binding<ViewModel>())
 class UpdatesSettingsViewModel(
-    val updatesPreferences: UpdatesPreferences = Injekt.get(),
-    val getCategories: GetMangaCategories = Injekt.get(),
+    val updatesPreferences: UpdatesPreferences,
+    val getCategories: GetMangaCategories,
 ) : ViewModel() {
 
     val includedCategories = updatesPreferences.filterIncludedCategories

app/src/main/java/eu/kanade/tachiyomi/util/chapter/ChapterFilterDownloaded.kt
@@ -4,16 +4,12 @@ import eu.kanade.tachiyomi.data.download.manga.MangaDownloadCache
 import tachiyomi.domain.entries.manga.model.Manga
 import tachiyomi.domain.items.chapter.model.Chapter
 import tachiyomi.source.local.entries.manga.isLocal
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
 /**
  * Returns a copy of the list with not downloaded chapters removed.
  */
-fun List<Chapter>.filterDownloadedChapters(manga: Manga): List<Chapter> {
+fun List<Chapter>.filterDownloaded(manga: Manga, downloadCache: MangaDownloadCache): List<Chapter> {
     if (manga.isLocal()) return this
-
-    val downloadCache: MangaDownloadCache = Injekt.get()
 
     return filter {
         downloadCache.isChapterDownloaded(
@@ -25,8 +21,4 @@ fun List<Chapter>.filterDownloadedChapters(manga: Manga): List<Chapter> {
             false,
         )
     }
-}
-
-fun List<Chapter>.filterDownloaded(manga: Manga): List<Chapter> {
-    return filterDownloadedChapters(manga)
 }