mihonapp/mihon

Make the source manager surface suspend (#3869)
Assisted-by: Claude:claude-opus-5

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupFileValidator.kt
@@ -17,7 +17,7 @@ class BackupFileValidator(
      *
      * @return List of missing sources or missing trackers.
      */
-    fun validate(uri: Uri): Results {
+    suspend fun validate(uri: Uri): Results {
         val backup = try {
             backupDecoder.decode(uri)
         } catch (e: Exception) {
@@ -26,7 +26,7 @@ class BackupFileValidator(
 
         val sources = backup.backupSources.associate { it.sourceId to it.name }
         val missingSources = sources
-            .filter { sourceManager.get(it.key) == null }
+            .filterKeys { sourceManager.get(it) == null }
             .values.map {
                 val id = it.toLongOrNull()
                 if (id == null) {

app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/manga/MigrateMangaViewModel.kt
@@ -12,6 +12,7 @@ import dev.zacsweers.metrox.viewmodel.ManualViewModelAssistedFactory
 import dev.zacsweers.metrox.viewmodel.ManualViewModelAssistedFactoryKey
 import eu.kanade.tachiyomi.source.Source
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -50,7 +51,7 @@ class MigrateMangaViewModel(
     private val _events: Channel<MigrationMangaEvent> = Channel()
     val events: Flow<MigrationMangaEvent> = _events.receiveAsFlow()
 
-    private val source by lazy { sourceManager.getOrStub(sourceId) }
+    private val source = viewModelScope.async { sourceManager.getOrStub(sourceId) }
 
     private val selection = MutableStateFlow(emptySet<Long>())
 
@@ -68,7 +69,7 @@ class MigrateMangaViewModel(
         favorites,
         selection,
     ) { titleList, selection ->
-        State(source = source, selection = selection, titleList = titleList)
+        State(source = source.await(), selection = selection, titleList = titleList)
     }
         .flowOn(Dispatchers.IO)
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5.seconds), State())

app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
@@ -35,6 +35,7 @@ import eu.kanade.tachiyomi.data.download.model.Download
 import eu.kanade.tachiyomi.data.saver.Image
 import eu.kanade.tachiyomi.data.saver.ImageSaver
 import eu.kanade.tachiyomi.data.saver.Location
+import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.ui.reader.loader.ChapterLoader
@@ -156,6 +157,12 @@ class ReaderViewModel(
         get() = state.value.manga
 
     /**
+     * The source of the manga loaded in the reader. Null until it has been resolved.
+     */
+    val source: Source?
+        get() = state.value.source
+
+    /**
      * The chapter id of the currently loaded chapter. Used to restore from process kill.
      */
     private var chapterId = savedState.get<Long>("chapter_id") ?: -1L
@@ -265,7 +272,7 @@ class ReaderViewModel(
             .map(::ReaderChapter)
     }
 
-    private val incognitoMode: Boolean by lazy { getIncognitoState.await(manga?.source) }
+    private var incognitoMode: Boolean = false
     private val downloadAheadAmount = downloadPreferences.autoDownloadWhileReading.get()
 
     init {
@@ -316,11 +323,11 @@ class ReaderViewModel(
         withIOContext {
             try {
                 val manga = getManga.await(mangaId) ?: error("Requested manga of id $mangaId not found")
-                sourceManager.isInitialized.first { it }
-                mutableState.update { it.copy(manga = manga) }
+                val source = sourceManager.getOrStub(manga.source)
+                incognitoMode = getIncognitoState.await(manga.source)
+                mutableState.update { it.copy(manga = manga, source = source) }
                 if (chapterId == -1L) chapterId = initialChapterId
 
-                val source = sourceManager.getOrStub(manga.source)
                 loader = ChapterLoader(context, downloadManager, downloadProvider, chapterCache, manga, source)
 
                 loadChapter(loader!!, chapterList.first { chapterId == it.chapter.id })
@@ -425,13 +432,13 @@ class ReaderViewModel(
         if (chapter.pageLoader?.isLocal == false) {
             val manga = manga ?: return
             val dbChapter = chapter.chapter
-            val isDownloaded = downloadManager.isChapterDownloaded(
+            val source = state.value.source ?: return
+            val isDownloaded = downloadManager.isChapterDownloadedOnDisk(
                 dbChapter.name,
                 dbChapter.scanlator,
                 dbChapter.url,
                 manga.title,
-                manga.source,
-                skipCache = true,
+                source,
             )
             if (isDownloaded) {
                 chapter.state = ReaderChapter.State.Wait
@@ -654,7 +661,7 @@ class ReaderViewModel(
         return state.value.currentChapter
     }
 
-    fun getSource() = manga?.source?.let { sourceManager.getOrStub(it) } as? HttpSource
+    fun getSource() = state.value.source as? HttpSource
 
     fun getChapterUrl(): String? {
         val sChapter = getCurrentChapter()?.chapter ?: return null
@@ -974,6 +981,7 @@ class ReaderViewModel(
     @Immutable
     data class State(
         val manga: Manga? = null,
+        val source: Source? = null,
         val initError: Throwable? = null,
         val viewerChapters: ViewerChapters? = null,
         val bookmarked: Boolean = false,

app/src/main/java/eu/kanade/tachiyomi/ui/webview/WebViewViewModel.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.ui.webview
 import android.content.Context
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.Assisted
 import dev.zacsweers.metro.AssistedFactory
@@ -15,6 +16,9 @@ import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.system.openInBrowser
 import eu.kanade.tachiyomi.util.system.toShareIntent
 import eu.kanade.tachiyomi.util.system.toast
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import logcat.LogPriority
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import tachiyomi.core.common.util.system.logcat
@@ -34,14 +38,18 @@ class WebViewViewModel(
         fun create(sourceId: Long?): WebViewViewModel
     }
 
-    var headers = emptyMap<String, String>()
+    /** Null until the source it belongs to has been resolved. */
+    val headers: StateFlow<Map<String, String>?>
+        field = MutableStateFlow<Map<String, String>?>(null)
 
     init {
-        sourceId?.let { sourceManager.get(it) as? HttpSource }?.let { source ->
-            try {
-                headers = source.headers.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }
+        viewModelScope.launch {
+            val source = sourceId?.let { sourceManager.get(it) as? HttpSource }
+            headers.value = try {
+                source?.headers?.toMultimap()?.mapValues { it.value.getOrNull(0) ?: "" }.orEmpty()
             } catch (e: Exception) {
                 logcat(LogPriority.ERROR, e) { "Failed to build headers" }
+                emptyMap()
             }
         }
     }

app/src/main/java/eu/kanade/tachiyomi/util/CrashLogUtil.kt
@@ -12,7 +12,6 @@ import eu.kanade.tachiyomi.util.system.WebViewUtil
 import eu.kanade.tachiyomi.util.system.createFileInCacheDir
 import eu.kanade.tachiyomi.util.system.toShareIntent
 import eu.kanade.tachiyomi.util.system.toast
-import kotlinx.coroutines.flow.first
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.offsetAt
 import kotlinx.datetime.toLocalDateTime
@@ -66,7 +65,7 @@ class CrashLogUtil(
     private suspend fun getExtensionsInfo(): String? {
         val availableExtensions = extensionManager.availableExtensionsFlow.value.associateBy { it.pkgName }
 
-        val extensionInfoList = extensionManager.installedExtensionsFlow.first()
+        val extensionInfoList = extensionManager.getInstalledExtensions()
             .sortedBy { it.name }
             .mapNotNull {
                 val availableExtension = availableExtensions[it.pkgName]

Animetailapp/Animetail

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupFileValidator.kt
@@ -19,7 +19,7 @@ class BackupFileValidator(
      *
      * @return List of missing sources or missing trackers.
      */
-    fun validate(uri: Uri): Results {
+    suspend fun validate(uri: Uri): Results {
         val backup = try {
             backupDecoder.decode(uri)
         } catch (e: Exception) {
@@ -29,7 +29,7 @@ class BackupFileValidator(
         val sources = backup.backupSources.associate { it.sourceId to it.name }
         val animesources = backup.backupAnimeSources.associate { it.sourceId to it.name }
         val missingSources = sources
-            .filter { mangaSourceManager.get(it.key) == null }
+            .filterKeys { mangaSourceManager.get(it) == null }
             .values.map {
                 val id = it.toLongOrNull()
                 if (id == null) {

app/src/main/java/eu/kanade/tachiyomi/ui/browse/manga/migration/manga/MigrateMangaViewModel.kt
@@ -12,6 +12,7 @@ import dev.zacsweers.metrox.viewmodel.ManualViewModelAssistedFactory
 import dev.zacsweers.metrox.viewmodel.ManualViewModelAssistedFactoryKey
 import eu.kanade.tachiyomi.source.MangaSource
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -49,7 +50,7 @@ class MigrateMangaViewModel(
     private val _events: Channel<MigrationMangaEvent> = Channel()
     val events: Flow<MigrationMangaEvent> = _events.receiveAsFlow()
 
-    private val source by lazy { sourceManager.getOrStub(sourceId) }
+    private val source = viewModelScope.async { sourceManager.getOrStub(sourceId) }
 
     private val selection = MutableStateFlow(emptySet<Long>())
 
@@ -67,7 +68,7 @@ class MigrateMangaViewModel(
         favorites,
         selection,
     ) { titleList, selection ->
-        State(source = source, selectedMangaIds = selection, titleList = titleList)
+        State(source = source.await(), selectedMangaIds = selection, titleList = titleList)
     }
         .flowOn(Dispatchers.IO)
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5.seconds), State())

app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
@@ -164,7 +164,13 @@ class ReaderViewModel(
         get() = state.value.manga
 
     val currentSource: MangaSource?
-        get() = manga?.source?.let { sourceManager.getOrStub(it) }
+        get() = state.value.source
+
+    /**
+     * The source of the manga loaded in the reader. Null until it has been resolved.
+     */
+    val source: MangaSource?
+        get() = state.value.source
 
     /**
      * The chapter id of the currently loaded chapter. Used to restore from process kill.
@@ -279,7 +285,8 @@ class ReaderViewModel(
             .map { ReaderChapter(it) }
     }
 
-    val incognitoMode: Boolean by lazy { getMangaIncognitoState.await(manga?.source) }
+    var incognitoMode: Boolean = false
+        private set
     private val downloadAheadAmount = downloadPreferences.autoDownloadWhileReading.get()
 
     init {
@@ -337,11 +344,11 @@ class ReaderViewModel(
         withIOContext {
             try {
                 val manga = getManga.await(mangaId) ?: error("Requested manga of id $mangaId not found")
-                sourceManager.isInitialized.first { it }
-                mutableState.update { it.copy(manga = manga) }
+                val source = sourceManager.getOrStub(manga.source)
+                incognitoMode = getMangaIncognitoState.await(manga.source)
+                mutableState.update { it.copy(manga = manga, source = source) }
                 if (chapterId == -1L) chapterId = initialChapterId
 
-                val source = sourceManager.getOrStub(manga.source)
                 loader = ChapterLoader(
                     context = context,
                     scope = viewModelScope,
@@ -457,13 +464,13 @@ class ReaderViewModel(
         if (chapter.pageLoader?.isLocal == false) {
             val manga = manga ?: return
             val dbChapter = chapter.chapter
-            val isDownloaded = downloadManager.isChapterDownloaded(
+            val source = state.value.source ?: return
+            val isDownloaded = downloadManager.isChapterDownloadedOnDisk(
                 dbChapter.name,
                 dbChapter.scanlator,
                 dbChapter.url,
                 manga.title,
-                manga.source,
-                skipCache = true,
+                source,
             )
             if (isDownloaded) {
                 chapter.state = ReaderChapter.State.Wait
@@ -703,7 +710,7 @@ class ReaderViewModel(
         return state.value.currentChapter
     }
 
-    fun getSource() = manga?.source?.let { sourceManager.getOrStub(it) } as? HttpSource
+    fun getSource() = state.value.source as? HttpSource
 
     fun getChapterUrl(): String? {
         val sChapter = getCurrentChapter()?.chapter ?: return null
@@ -1162,6 +1169,7 @@ class ReaderViewModel(
     @Immutable
     data class State(
         val manga: Manga? = null,
+        val source: MangaSource? = null,
         val initError: Throwable? = null,
         val viewerChapters: ViewerChapters? = null,
         val bookmarked: Boolean = false,

app/src/main/java/eu/kanade/tachiyomi/ui/webview/WebViewViewModel.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.ui.webview
 import android.content.Context
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.Assisted
 import dev.zacsweers.metro.AssistedFactory
@@ -16,6 +17,9 @@ import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.system.openInBrowser
 import eu.kanade.tachiyomi.util.system.toShareIntent
 import eu.kanade.tachiyomi.util.system.toast
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import logcat.LogPriority
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import tachiyomi.core.common.util.system.logcat
@@ -37,21 +41,20 @@ class WebViewViewModel(
         fun create(sourceId: Long?): WebViewViewModel
     }
 
-    var headers = emptyMap<String, String>()
+    /** Null until the source it belongs to has been resolved. */
+    val headers: StateFlow<Map<String, String>?>
+        field = MutableStateFlow<Map<String, String>?>(null)
 
     init {
-        sourceId?.let { mangaSourceManager.get(it) as? HttpSource }?.let { mangasource ->
-            try {
-                headers = mangasource.headers.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }
+        viewModelScope.launch {
+            val mangaSource = sourceId?.let { mangaSourceManager.get(it) as? HttpSource }
+            val animeSource = sourceId?.let { animeSourceManager.get(it) as? AnimeHttpSource }
+            headers.value = try {
+                mangaSource?.headers?.toMultimap()?.mapValues { it.value.getOrNull(0) ?: "" }
+                    ?: animeSource?.headers?.toMultimap()?.mapValues { it.value.getOrNull(0) ?: "" }.orEmpty()
             } catch (e: Exception) {
                 logcat(LogPriority.ERROR, e) { "Failed to build headers" }
-            }
-        }
-        sourceId?.let { animeSourceManager.get(it) as? AnimeHttpSource }?.let { animesource ->
-            try {
-                headers = animesource.headers.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }
-            } catch (e: Exception) {
-                logcat(LogPriority.ERROR, e) { "Failed to build headers" }
+                emptyMap()
             }
         }
     }

app/src/main/java/eu/kanade/tachiyomi/util/CrashLogUtil.kt
@@ -78,7 +78,7 @@ class CrashLogUtil(
     private suspend fun getMangaExtensionsInfo(): String? {
         val availableExtensions = mangaExtensionManager.availableExtensionsFlow.value.associateBy { it.pkgName }
 
-        val extensionInfoList = mangaExtensionManager.installedExtensionsFlow.first()
+        val extensionInfoList = mangaExtensionManager.getInstalledExtensions()
             .sortedBy { it.name }
             .mapNotNull {
                 val availableExtension = availableExtensions[it.pkgName]