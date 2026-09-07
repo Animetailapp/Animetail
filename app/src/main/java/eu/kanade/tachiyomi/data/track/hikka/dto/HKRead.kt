mihonapp/mihon

Minor tracking refactors (#3900)
* Make tracker Api classes take their tracker's ID

Also made the name consistently `trackerId` since `trackId` can be
confusing in the context of Tracks that also carry several IDs.

Also made the public constants in TrackerManager entirely redundant,
so trackers are all equally getting their ID defined in their
constructor call now.

* Explicitly mark unused Exceptions as such

* Make isExpired method of BGMOAuth data class

As opposed to an extension defined in the same file

* Kavita: thumbnail_url -> thumbnailUrl

With a SerialName annotation of course

* Suwayomi: Remove redundant with(json)

Both of these requests don't use parseAs so this not required.

* Bangumi: Don't recreate OAuth object for storing

* Remove unused attribute from tracker OAuth classes

Mostly `token_type`.
Also removed `user_id` from BGMOAuth and `scope` & `expiresIn` from
MangaBakaOAuth (which already provides `expiresAt`).

* Use kotlin.time durations for token expiry math

Something like `Clock.System.now().plus(1.hours)` is much easier to
read than `System.getEpochMillis() + 3600`.

Also lets us use the `epochSeconds` attribute where the given
timestamps are seconds-resolution.

* Bangumi: Remove redundant apply block

* Kavita: Misc simplifications

* Use parseAs in tracker interceptors

* MAL: Use existing setAuth to store refresh token

app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/dto/HKRead.kt
@@ -24,8 +24,8 @@ data class HKRead(
     val endDate: Long? = null,
     val content: HKManga? = null,
 ) {
-    fun toTrack(trackId: Long): TrackSearch {
-        return TrackSearch.create(trackId).apply {
+    fun toTrack(trackerId: Long): TrackSearch {
+        return TrackSearch.create(trackerId).apply {
             val mangaContent = this@HKRead.content
             if (mangaContent != null) {
                 title = mangaContent.titleUa ?: mangaContent.titleEn ?: mangaContent.titleOriginal

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/dto/SMUserListEntry.kt
@@ -24,8 +24,8 @@ data class SMUserListManga(
     val totalChapters: Long, // the title's total chapters
     val userRate: SMUserRate?,
 ) {
-    fun toTrack(trackId: Long): Track {
-        return Track.create(trackId).apply {
+    fun toTrack(trackerId: Long): Track {
+        return Track.create(trackerId).apply {
             title = name
             total_chapters = totalChapters
             tracking_url = url

Animetailapp/Animetail

app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/dto/HKRead.kt
@@ -25,8 +25,8 @@ data class HKRead(
     val endDate: Long? = null,
     val content: HKManga? = null,
 ) {
-    fun toTrack(trackId: Long): MangaTrackSearch {
-        return MangaTrackSearch.create(trackId).apply {
+    fun toTrack(trackerId: Long): MangaTrackSearch {
+        return MangaTrackSearch.create(trackerId).apply {
             val mangaContent = this@HKRead.content
             if (mangaContent != null) {
                 title = mangaContent.titleUa ?: mangaContent.titleEn ?: mangaContent.titleOriginal
@@ -45,8 +45,8 @@ data class HKRead(
         }
     }
 
-    fun toAnimeTrack(trackId: Long): AnimeTrackSearch {
-        return AnimeTrackSearch.create(trackId).apply {
+    fun toAnimeTrack(trackerId: Long): AnimeTrackSearch {
+        return AnimeTrackSearch.create(trackerId).apply {
             val mangaContent = this@HKRead.content
             if (mangaContent != null) {
                 title = mangaContent.titleUa ?: mangaContent.titleEn ?: mangaContent.titleOriginal

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/dto/SMUserListEntry.kt
@@ -34,8 +34,8 @@ data class SMUserListManga(
     val totalChapters: Long,
     val userRate: SMUserRate?,
 ) {
-    fun toTrack(trackId: Long): MangaTrack {
-        return MangaTrack.create(trackId).apply {
+    fun toTrack(trackerId: Long): MangaTrack {
+        return MangaTrack.create(trackerId).apply {
             title = name
             total_chapters = totalChapters
             tracking_url = url
@@ -69,8 +69,8 @@ data class SMAnimeUserListAnime(
     val totalEpisodes: Long,
     val userRate: SMAnimeUserRate?,
 ) {
-    fun toTrack(trackId: Long): AnimeTrack {
-        return AnimeTrack.create(trackId).apply {
+    fun toTrack(trackerId: Long): AnimeTrack {
+        return AnimeTrack.create(trackerId).apply {
             title = name
             total_episodes = totalEpisodes
             tracking_url = url