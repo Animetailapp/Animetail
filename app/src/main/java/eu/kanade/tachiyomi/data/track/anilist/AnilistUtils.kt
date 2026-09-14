mihonapp/mihon

Make tracker utils package-internal (#3908)
It gets really annoying to use any of them when they have name
collisions. Especially extensions on Track or basic public functions.

Also make some same-file extension function in the MangaUpdates
package method on the class they extended.

I would have done the same for the DTOs themselves but any that are
used on method signatures have to be public and then all their members
also have to be public. We'd end up with a confusing mess of some
internal and some public DTOs, even at the top level (since generics
can be internal to the `dto` package (e.g. with parseAs<...>) but
return annotations cannot be).

app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/AnilistUtils.kt
@@ -4,7 +4,7 @@ import eu.kanade.domain.track.service.TrackPreferences
 import eu.kanade.tachiyomi.data.database.models.Track
 import tachiyomi.domain.track.model.Track as DomainTrack
 
-fun Track.toApiStatus() = when (status) {
+internal fun Track.toApiStatus() = when (status) {
     Anilist.READING -> "CURRENT"
     Anilist.COMPLETED -> "COMPLETED"
     Anilist.ON_HOLD -> "PAUSED"
@@ -14,7 +14,7 @@ fun Track.toApiStatus() = when (status) {
     else -> throw NotImplementedError("Unknown status: $status")
 }
 
-fun DomainTrack.toApiScore(preferences: TrackPreferences): String = when (preferences.anilistScoreType.get()) {
+internal fun DomainTrack.toApiScore(preferences: TrackPreferences): String = when (preferences.anilistScoreType.get()) {
     // 10 point
     "POINT_10" -> (score.toInt() / 10).toString()
     // 100 point

app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/HikkaUtils.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.data.track.hikka
 import eu.kanade.tachiyomi.data.database.models.Track
 import java.util.UUID
 
-fun Track.toApiStatus() = when (status) {
+internal fun Track.toApiStatus() = when (status) {
     Hikka.READING -> "reading"
     Hikka.COMPLETED -> "completed"
     Hikka.ON_HOLD -> "on_hold"
@@ -13,7 +13,7 @@ fun Track.toApiStatus() = when (status) {
     else -> throw NotImplementedError("Hikka: Unknown status: $status")
 }
 
-fun toTrackStatus(status: String) = when (status) {
+internal fun toTrackStatus(status: String) = when (status) {
     "reading" -> Hikka.READING
     "completed" -> Hikka.COMPLETED
     "on_hold" -> Hikka.ON_HOLD
@@ -22,7 +22,7 @@ fun toTrackStatus(status: String) = when (status) {
     else -> throw NotImplementedError("Hikka: Unknown status: $status")
 }
 
-fun stringToNumber(input: String): Long {
+internal fun stringToNumber(input: String): Long {
     val uuid = UUID.nameUUIDFromBytes(input.toByteArray())
     return uuid.mostSignificantBits and Long.MAX_VALUE
 }

app/src/main/java/eu/kanade/tachiyomi/data/track/kitsu/KitsuUtils.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.data.track.kitsu
 
 import eu.kanade.tachiyomi.data.database.models.Track
 
-fun Track.toKitsuApiStatus() = when (status) {
+internal fun Track.toKitsuApiStatus() = when (status) {
     Kitsu.READING -> "CURRENT"
     Kitsu.COMPLETED -> "COMPLETED"
     Kitsu.ON_HOLD -> "ON_HOLD"
@@ -11,7 +11,7 @@ fun Track.toKitsuApiStatus() = when (status) {
     else -> throw Exception("Unknown status: $status")
 }
 
-fun String.toKitsuLocalStatus() = when (this) {
+internal fun String.toKitsuLocalStatus() = when (this) {
     "CURRENT" -> Kitsu.READING
     "COMPLETED" -> Kitsu.COMPLETED
     "ON_HOLD" -> Kitsu.ON_HOLD

app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBakaUtils.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.data.track.mangabaka
 
 import eu.kanade.tachiyomi.data.database.models.Track
 
-fun Track.toApiStatus() = when (status) {
+internal fun Track.toApiStatus() = when (status) {
     MangaBaka.CONSIDERING -> "considering"
     MangaBaka.COMPLETED -> "completed"
     MangaBaka.DROPPED -> "dropped"

app/src/main/java/eu/kanade/tachiyomi/data/track/mangaupdates/dto/MURating.kt
@@ -6,10 +6,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MURating(
     val rating: Double? = null,
-)
-
-fun MURating.copyTo(track: Track): Track {
-    return track.apply {
-        this.score = rating ?: 0.0
+) {
+    fun copyTo(track: Track): Track {
+        return track.apply {
+            this.score = rating ?: 0.0
+        }
     }
 }

app/src/main/java/eu/kanade/tachiyomi/data/track/myanimelist/MyAnimeListUtils.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.data.track.myanimelist
 
 import eu.kanade.tachiyomi.data.database.models.Track
 
-fun Track.toMyAnimeListStatus() = when (status) {
+internal fun Track.toMyAnimeListStatus() = when (status) {
     MyAnimeList.READING -> "reading"
     MyAnimeList.COMPLETED -> "completed"
     MyAnimeList.ON_HOLD -> "on_hold"
@@ -12,7 +12,7 @@ fun Track.toMyAnimeListStatus() = when (status) {
     else -> null
 }
 
-fun getStatus(status: String?) = when (status) {
+internal fun getStatus(status: String?) = when (status) {
     "reading" -> MyAnimeList.READING
     "completed" -> MyAnimeList.COMPLETED
     "on_hold" -> MyAnimeList.ON_HOLD

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/ShikimoriUtils.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.data.track.shikimori
 
 import eu.kanade.tachiyomi.data.database.models.Track
 
-fun Track.toShikimoriStatus() = when (status) {
+internal fun Track.toShikimoriStatus() = when (status) {
     Shikimori.READING -> "watching"
     Shikimori.COMPLETED -> "completed"
     Shikimori.ON_HOLD -> "on_hold"
@@ -12,7 +12,7 @@ fun Track.toShikimoriStatus() = when (status) {
     else -> throw NotImplementedError("Unknown status: $status")
 }
 
-fun toTrackStatus(status: String) = when (status) {
+internal fun toTrackStatus(status: String) = when (status) {
     "watching" -> Shikimori.READING
     "completed" -> Shikimori.COMPLETED
     "on_hold" -> Shikimori.ON_HOLD

Animetailapp/Animetail

app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/AnilistUtils.kt
@@ -6,7 +6,7 @@ import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
 import tachiyomi.domain.track.anime.model.AnimeTrack as DomainAnimeTrack
 import tachiyomi.domain.track.manga.model.MangaTrack as DomainMangaTrack
 
-fun MangaTrack.toApiStatus() = when (status) {
+internal fun MangaTrack.toApiStatus() = when (status) {
     Anilist.READING -> "CURRENT"
     Anilist.COMPLETED -> "COMPLETED"
     Anilist.ON_HOLD -> "PAUSED"
@@ -16,7 +16,7 @@ fun MangaTrack.toApiStatus() = when (status) {
     else -> throw NotImplementedError("Unknown status: $status")
 }
 
-fun AnimeTrack.toApiStatus() = when (status) {
+internal fun AnimeTrack.toApiStatus() = when (status) {
     Anilist.WATCHING -> "CURRENT"
     Anilist.COMPLETED -> "COMPLETED"
     Anilist.ON_HOLD -> "PAUSED"
@@ -57,5 +57,5 @@ private fun Double.toApiScore(preferences: TrackPreferences): String = when (pre
     else -> throw Exception("Unknown score type")
 }
 
-fun DomainMangaTrack.toApiScore(preferences: TrackPreferences): String = this.score.toApiScore(preferences)
-fun DomainAnimeTrack.toApiScore(preferences: TrackPreferences): String = this.score.toApiScore(preferences)
+internal fun DomainMangaTrack.toApiScore(preferences: TrackPreferences): String = this.score.toApiScore(preferences)
+internal fun DomainAnimeTrack.toApiScore(preferences: TrackPreferences): String = this.score.toApiScore(preferences)

app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/HikkaUtils.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.data.database.models.anime.AnimeTrack
 import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
 import java.util.UUID
 
-fun MangaTrack.toApiStatus() = when (status) {
+internal fun MangaTrack.toApiStatus() = when (status) {
     Hikka.READING -> "reading"
     Hikka.COMPLETED -> "completed"
     Hikka.ON_HOLD -> "on_hold"
@@ -14,7 +14,7 @@ fun MangaTrack.toApiStatus() = when (status) {
     else -> throw NotImplementedError("Hikka: Unknown status: $status")
 }
 
-fun AnimeTrack.toApiStatus() = when (status) {
+internal fun AnimeTrack.toApiStatus() = when (status) {
     Hikka.WATCHING -> "reading"
     Hikka.COMPLETED -> "completed"
     Hikka.ON_HOLD -> "on_hold"
@@ -24,7 +24,7 @@ fun AnimeTrack.toApiStatus() = when (status) {
     else -> throw NotImplementedError("Hikka: Unknown status: $status")
 }
 
-fun toTrackStatus(status: String) = when (status) {
+internal fun toTrackStatus(status: String) = when (status) {
     "reading" -> Hikka.READING
     "completed" -> Hikka.COMPLETED
     "on_hold" -> Hikka.ON_HOLD
@@ -33,7 +33,7 @@ fun toTrackStatus(status: String) = when (status) {
     else -> throw NotImplementedError("Hikka: Unknown status: $status")
 }
 
-fun stringToNumber(input: String): Long {
+internal fun stringToNumber(input: String): Long {
     val uuid = UUID.nameUUIDFromBytes(input.toByteArray())
     return uuid.mostSignificantBits and Long.MAX_VALUE
 }

app/src/main/java/eu/kanade/tachiyomi/data/track/kitsu/KitsuUtils.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.data.track.kitsu
 import eu.kanade.tachiyomi.data.database.models.anime.AnimeTrack
 import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
 
-fun MangaTrack.toKitsuApiStatus() = when (status) {
+internal fun MangaTrack.toKitsuApiStatus() = when (status) {
     Kitsu.READING -> "CURRENT"
     Kitsu.COMPLETED -> "COMPLETED"
     Kitsu.ON_HOLD -> "ON_HOLD"
@@ -12,7 +12,7 @@ fun MangaTrack.toKitsuApiStatus() = when (status) {
     else -> throw Exception("Unknown status: $status")
 }
 
-fun AnimeTrack.toKitsuApiStatus() = when (status) {
+internal fun AnimeTrack.toKitsuApiStatus() = when (status) {
     Kitsu.WATCHING -> "CURRENT"
     Kitsu.COMPLETED -> "COMPLETED"
     Kitsu.ON_HOLD -> "ON_HOLD"
@@ -21,7 +21,7 @@ fun AnimeTrack.toKitsuApiStatus() = when (status) {
     else -> throw Exception("Unknown status: $status")
 }
 
-fun String.toKitsuLocalStatus() = when (this) {
+internal fun String.toKitsuLocalStatus() = when (this) {
     "CURRENT" -> Kitsu.READING
     "COMPLETED" -> Kitsu.COMPLETED
     "ON_HOLD" -> Kitsu.ON_HOLD
@@ -30,7 +30,7 @@ fun String.toKitsuLocalStatus() = when (this) {
     else -> throw Exception("Unknown status: $this")
 }
 
-fun String.toKitsuLocalStatusAnime() = when (this) {
+internal fun String.toKitsuLocalStatusAnime() = when (this) {
     "CURRENT" -> Kitsu.WATCHING
     "COMPLETED" -> Kitsu.COMPLETED
     "ON_HOLD" -> Kitsu.ON_HOLD

app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBakaUtils.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.data.track.mangabaka
 
 import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
 
-fun MangaTrack.toApiStatus() = when (status) {
+internal fun MangaTrack.toApiStatus() = when (status) {
     MangaBaka.CONSIDERING -> "considering"
     MangaBaka.COMPLETED -> "completed"
     MangaBaka.DROPPED -> "dropped"

app/src/main/java/eu/kanade/tachiyomi/data/track/mangaupdates/dto/MURating.kt
@@ -6,10 +6,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MURating(
     val rating: Double? = null,
-)
-
-fun MURating.copyTo(track: MangaTrack): MangaTrack {
-    return track.apply {
-        this.score = rating ?: 0.0
+) {
+    fun copyTo(track: MangaTrack): MangaTrack {
+        return track.apply {
+            this.score = rating ?: 0.0
+        }
     }
 }

app/src/main/java/eu/kanade/tachiyomi/data/track/myanimelist/MyAnimeListUtils.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.data.track.myanimelist
 import eu.kanade.tachiyomi.data.database.models.anime.AnimeTrack
 import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
 
-fun MangaTrack.toMyAnimeListStatus() = when (status) {
+internal fun MangaTrack.toMyAnimeListStatus() = when (status) {
     MyAnimeList.READING -> "reading"
     MyAnimeList.COMPLETED -> "completed"
     MyAnimeList.ON_HOLD -> "on_hold"
@@ -13,7 +13,7 @@ fun MangaTrack.toMyAnimeListStatus() = when (status) {
     else -> null
 }
 
-fun AnimeTrack.toMyAnimeListStatus() = when (status) {
+internal fun AnimeTrack.toMyAnimeListStatus() = when (status) {
     MyAnimeList.WATCHING -> "watching"
     MyAnimeList.READING -> "watching"
     MyAnimeList.COMPLETED -> "completed"
@@ -26,7 +26,7 @@ fun AnimeTrack.toMyAnimeListStatus() = when (status) {
     else -> null
 }
 
-fun getStatus(status: String?) = when (status) {
+internal fun getStatus(status: String?) = when (status) {
     "reading" -> MyAnimeList.READING
     "watching" -> MyAnimeList.WATCHING
     "completed" -> MyAnimeList.COMPLETED

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/ShikimoriUtils.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.data.track.shikimori
 import eu.kanade.tachiyomi.data.database.models.anime.AnimeTrack
 import eu.kanade.tachiyomi.data.database.models.manga.MangaTrack
 
-fun MangaTrack.toShikimoriStatus() = when (status) {
+internal fun MangaTrack.toShikimoriStatus() = when (status) {
     Shikimori.READING -> "watching"
     Shikimori.COMPLETED -> "completed"
     Shikimori.ON_HOLD -> "on_hold"
@@ -13,7 +13,7 @@ fun MangaTrack.toShikimoriStatus() = when (status) {
     else -> throw NotImplementedError("Unknown status: $status")
 }
 
-fun AnimeTrack.toShikimoriStatus() = when (status) {
+internal fun AnimeTrack.toShikimoriStatus() = when (status) {
     Shikimori.READING -> "watching"
     Shikimori.COMPLETED -> "completed"
     Shikimori.ON_HOLD -> "on_hold"
@@ -23,7 +23,7 @@ fun AnimeTrack.toShikimoriStatus() = when (status) {
     else -> throw NotImplementedError("Unknown status: $status")
 }
 
-fun toTrackStatus(status: String) = when (status) {
+internal fun toTrackStatus(status: String) = when (status) {
     "watching" -> Shikimori.READING
     "completed" -> Shikimori.COMPLETED
     "on_hold" -> Shikimori.ON_HOLD